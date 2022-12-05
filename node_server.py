164163f4b93a9d74eea09e7498e4f3265c47ebef
Android Developer <developer79433@gmail.com>
2022-12-05 18:50:01 +1100

    Move class Block into a separate file.

--- node_server.py
+++ node_server.py
@@ -2,7 +2,6 @@ import os
 import sys
 import signal
 import atexit
-from hashlib import sha256
 import json
 import time
 import logging
@@ -10,23 +9,10 @@ import logging
 from flask import Flask, request
 import requests
 
+from blockchain import Block
+
 
 logging.basicConfig(level=logging.DEBUG)
-
-class Block:
-    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
-        self.index = index
-        self.transactions = transactions
-        self.timestamp = timestamp
-        self.previous_hash = previous_hash
-        self.nonce = nonce
-
-    def compute_hash(self):
-        """
-        A function that return the hash of the block contents.
-        """
-        block_string = json.dumps(self.__dict__, sort_keys=True)
-        return sha256(block_string.encode()).hexdigest()
 
 
 class Blockchain:
